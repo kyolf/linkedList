2695ce991731c1953640d3394eea3e4f6ea8b61c
williamtwobit <william@twobit.cc>
2017-07-27 11:29:05 -0400

    insert methof implemented

--- linkedList.js
+++ linkedList.js
@@ -6,6 +6,31 @@ class LinkedList {
     this.length=0;
   }
 
+  insert(index, value){
+    if (index < 0 || index > this.length) {
+      throw new Error('Index error');
+    }
+
+    let node = this.head;
+
+    let newNode = {
+      value
+    };
+
+    if(index === 0){
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    else{
+      let prevNode = this._find(index-1);
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+    }
+
+    this.length++;
+  }
+
   _find(index){
     let node = this.head;
     for(let i=0; i < index;i++){
